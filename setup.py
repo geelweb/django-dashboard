c87f4e34e996e93dc4f701f93362320ae18550c3
Guillaume Luchet <guillaume@geelweb.org>
2015-03-08 11:33:11 +0100

    upgrade version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ Seupt script to install django-dashboard
 """
 
 __author__ = "Guillaume Luchet <guillaume@geelweb.org>"
-__version__ = "0.1"
+__version__ = "0.1.1"
 
 import os, sys
 from setuptools import setup, find_packages
